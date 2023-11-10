0c9972ad092a3623fd206f7fb7aeb425a4c5fc58
unknown <b0n3yard@github.com>
2023-11-09 20:18:44 -0500

    installable atempting deploy

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -6,24 +6,42 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-const is_prod = process.env.NODE_ENV === 'production'
+const is_prod = process.env.NODE_ENV.trim() === 'production'
+console.log(process.env.NODE_ENV)
+
 const plugins = [
   new HtmlWebpackPlugin({
-    template:'./index.html',
+    template: './index.html',
     title: 'text'
   }),
 ]
-if(is_prod){
- plugins.push(...[
-  new InjectManifest({
-    swSrc: './src/sw-js',
-    swDest: 'src-sw.js'
-  })
- ])
+if (is_prod) {
+
+  plugins.push(...[
+    new WebpackPwaManifest({
+      name: 'jate editor',
+      short_name: 'jate_ed',
+      description: 'this is a progressive web app that can run in the browser both online and offline',
+      background_color: '#fff',
+      theme_color: '#2F4ced',
+      publicPath:'/',
+      inject:true,
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256.384, 512]
+        }
+      ],
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js'
+    })
+  ])
 }
 module.exports = () => {
   return {
-    mode: is_prod? 'production': 'development',
+    mode: is_prod ? 'production' : 'development',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -37,15 +55,15 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test:/\.css$/,
-          use:['style-loader', 'css-loader']
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         },
         {
-          test:/\.js$/,
-          exclude:/node_modules/,
-          use:{
-            loader:'babel-loader',
-            options:{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
               presets: ['@babel/preset-env']
             }
           }
